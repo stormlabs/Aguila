d40e814ddd070cdf39fda34eaed500a6d770b460
Carlos Zúñiga <carlos.zun@gmail.com>
2012-01-10 18:43:38 -0500

    Added User Entity and FOSUserBundle configuration

---
+++ src/Storm/AguilaBundle/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Storm\AguilaBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Storm\AguilaBundle\Entity\User
+ *
+ * @ORM\Table(name="aguila_user")
+ * @ORM\Entity(repositoryClass="Storm\AguilaBundle\Entity\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected  $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
