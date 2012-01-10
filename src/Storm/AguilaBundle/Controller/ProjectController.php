e83bb203d7692b9016017f363ffde98da1815c87
Ernesto Jose Vargas Paz <ejosvp@gmail.com>
2012-01-09 20:27:18 -0500

    added Project CRUD

---
+++ src/Storm/AguilaBundle/Controller/ProjectController.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Storm\AguilaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Storm\AguilaBundle\Entity\Project;
+use Storm\AguilaBundle\Form\ProjectType;
+
+/**
+ * Project controller.
+ *
+ * @Route("/project")
+ */
+class ProjectController extends Controller
+{
+    /**
+     * Lists all Project entities.
+     *
+     * @Route("/", name="project")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entities = $em->getRepository('AguilaBundle:Project')->findAll();
+
+        return array('entities' => $entities);
+    }
+
+    /**
+     * Finds and displays a Project entity.
+     *
+     * @Route("/{id}/show", name="project_show")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('AguilaBundle:Project')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Project entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),        );
+    }
+
+    /**
+     * Displays a form to create a new Project entity.
+     *
+     * @Route("/new", name="project_new")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Project();
+        $form   = $this->createForm(new ProjectType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView()
+        );
+    }
+
+    /**
+     * Creates a new Project entity.
+     *
+     * @Route("/create", name="project_create")
+     * @Method("post")
+     * @Template("AguilaBundle:Project:new.html.twig")
+     */
+    public function createAction()
+    {
+        $entity  = new Project();
+        $request = $this->getRequest();
+        $form    = $this->createForm(new ProjectType(), $entity);
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('project_show', array('id' => $entity->getId())));
+            
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView()
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Project entity.
+     *
+     * @Route("/{id}/edit", name="project_edit")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('AguilaBundle:Project')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Project entity.');
+        }
+
+        $editForm = $this->createForm(new ProjectType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Project entity.
+     *
+     * @Route("/{id}/update", name="project_update")
+     * @Method("post")
+     * @Template("AguilaBundle:Project:edit.html.twig")
+     */
+    public function updateAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('AguilaBundle:Project')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Project entity.');
+        }
+
+        $editForm   = $this->createForm(new ProjectType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        $request = $this->getRequest();
+
+        $editForm->bindRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('project_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a Project entity.
+     *
+     * @Route("/{id}/delete", name="project_delete")
+     * @Method("post")
+     */
+    public function deleteAction($id)
+    {
+        $form = $this->createDeleteForm($id);
+        $request = $this->getRequest();
+
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $entity = $em->getRepository('AguilaBundle:Project')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Project entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('project'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Storm/AguilaBundle/Form/ProjectType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Storm\AguilaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function getName()
+    {
+        return 'storm_aguilabundle_projecttype';
+    }
+}
