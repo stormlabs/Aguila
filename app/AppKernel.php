3fa035095a9af68340c1d03007389e7096d907c7
Ernesto Jose Vargas Paz <ejosvp@gmail.com>
2012-01-09 20:15:53 -0500

    added aguila bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Storm\AguilaBundle\AguilaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Storm/AguilaBundle/AguilaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Storm\AguilaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AguilaBundle extends Bundle
+{
+}
