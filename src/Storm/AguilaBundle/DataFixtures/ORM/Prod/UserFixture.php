12e13b1ff015cec012ea037754feefa65810536c
Ernesto Jose Vargas Paz <ejosvp@gmail.com>
2012-01-21 14:37:23 -0500

    added production fixtures

---
+++ src/Storm/AguilaBundle/DataFixtures/ORM/Prod/UserFixture.php
@@ -0,0 +1,48 @@
+<?php
+namespace Storm\AguilaBundle\DataFixtures\ORM\Prod;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Storm\AguilaBundle\Entity\User;
+
+class UserFixture extends AbstractFixture implements ContainerAwareInterface, OrderedFixtureInterface
+{
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param object $manager
+     */
+    public function load($manager)
+    {
+        /* @var $manipulator \FOS\UserBundle\Util\UserManipulator */
+        $manipulator = $this->container->get('fos_user.util.user_manipulator');
+
+        $manipulator->create(
+            'admin',
+            'admin',
+            'ejosblog@gmail.com',
+            true,
+            true);
+        $manipulator->promote('admin');
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    function getOrder()
+    {
+        return 1;
+    }
+
+}
