5f6acda96b00538b84f7708f092cdd1e070a4e9d
Carlos Zúñiga <carlos.zun@gmail.com>
2012-01-30 17:44:22 -0500

    Starting up the Project administration form

---
+++ src/Storm/AguilaBundle/Form/ProjectAdminType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Storm\AguilaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+
+class ProjectAdminType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('user', 'entity', array(
+                'class' => 'AguilaBundle:User',
+                'required' => false,
+            ))
+            ->add('permission', 'choice', array(
+                'choices' => array(
+                    MaskBuilder::MASK_VIEW => 'view',
+                    MaskBuilder::MASK_EDIT => 'edit',
+                )
+            ))
+        ;
+    }
+
+    public function getName()
+    {
+        return 'project_admin_form';
+    }
+}
